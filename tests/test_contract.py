023779adddeb86beda2e92bcbfa99c6f900c146a
nmcdonald96-tech8 <jeremyconnor900@yahoo.com>
2026-08-31 19:48:36 +0000

    Organize tests and remove generated Python caches

--- tests/test_contract.py
+++ tests/test_contract.py
@@ -1,3 +1,3 @@
-from services.progression_service import progression_clarity,simulate_progression,invalidate_progression_settings_cache,get_effective_progression_settings,classify_set_progression,calculate_set_specific_progression
-from services.workout_service import WorkoutStateService,workout_progress
-def test_contract(): assert callable(progression_clarity) and callable(workout_progress) and WorkoutStateService.active_set_position([{"done":False}])==(1,1)
+from services.progression_service import invalidate_progression_settings_cache,get_effective_progression_settings,classify_set_progression,calculate_set_specific_progression
+from services.workout_service import WorkoutStateService
+def test_contract(): assert callable(calculate_set_specific_progression) and WorkoutStateService.first_incomplete_set([{"done":False}])==0
