79c9becacd94a3f5c0efb079ba2acdc2c6f2e836
nmcdonald96-tech9 <jeremyconnor901@yahoo.com>
2026-09-10 15:26:34 -0400

    Delete application.py

--- application.py
+++
@@ -1,6 +0,0 @@
-from app.state import WorkoutState
-from app.router import ViewRouter
-class ApplicationFoundation:
-    def __init__(self, refresh_callback=None):
-        self.state=WorkoutState(); self.router=ViewRouter(self.state, refresh_callback)
-    def sync(self, controller): return self.state.sync_from_controller(controller)
